f040ca0727505fb18e57d9fb561f12a3ecd792d2
ariunzulm <munkhbatt.ariunzul@gmail.com>
2025-12-09 17:43:54 +0800

    commit

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const myTextElement = document.getElementById("myText");
 const myButtonElement = document.getElementById("myButton");
 const form = document.getElementById("form");
 const taskContainerElement = document.getElementById("task-container");
+const myPElement = d
+
 // ----TO GET INPUT-----------
-const tasks = [];
+let tasks = [];
 let taskId = 0;
 
 const addANewTask = () => {
@@ -22,29 +24,70 @@ const addANewTask = () => {
   cleanInput();
   renderTask();
 };
+
 const cleanInput = () => {
   myTextElement.value = "";
 };
+
+// ----TO STORE TASKS----------
 const createTask = (task) => {
-  return `<div class='task'>
+  return `<div class='task task-container'>
       <input type="checkbox"/>
       <p id="myP">${task.text}</p>
-      <button id="myDeleteButton">Delete</button>
+      <button id="myDeleteButton" onclick="toDeleteTasks(${task.id})">Delete</button>
     </div>`;
 };
 const renderTask = () => {
   let taskElements = "";
+
   // sending each element and add on top of it
   tasks.forEach((task) => {
     const taskEl = createTask(task);
+    console.log(taskEl, "taskel");
+
     taskElements += taskEl;
     taskContainerElement.innerHTML = taskElements;
   });
 };
-
-// ----TO STORE TASKS----------
-
 myButtonElement.addEventListener("click", addANewTask);
+
+// ----TO DELETE TASKS----------
+const toDeleteTasks = (id) => {
+  for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i].id === id) {
+      tasks.splice(i, 1);
+    }
+  }
+  renderTask();
+};
+
+function lineThrough(){
+
+}
+
+// ----
+
+// ----------FILTER----------------------
+// const deleteTodoId = 1;
+
+// const todos = [
+//   { id: 1, text: "hool" },
+//   { id: 2, text: "dleguur oroh" },
+// ];
+
+// const toDelete = todos.filter((todo) => {
+//   if (todo.id === deleteTodoId) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// hervee todo-iin id ni deleteTodoId tai tentsuu bol return true
+// esvel return false
+// });
+// console.log(toDelete);
+
+// [{ id: 1, text: "hool" }];
+// ------------------------------------
 // form.addEventListener("click", (e) => {
 //   e.preventDefault();
 // });
